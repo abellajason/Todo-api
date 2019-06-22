436eec2eee17d56f86d1c95511e8474ddb97e2af
Jason Abella <abellajason20@gmail.com>
2019-06-23 05:51:48 +0800

    updaets

--- server.js
+++ server.js
@@ -1,11 +1,44 @@
 var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
+var todos = [{
+    id : 1, 
+    description : "Meet mom for lunch",
+    completed : false
+}, {
+    id : 2,
+    description : "Go to Market",
+    completed : false
+}, {
+    id : 3,
+    description : "Feed the fish",
+    completed : true
+}];
 
 
 app.get('/',(req, res)=>{
     res.send('Todo API Root');
 });
+
+app.get('/todos', (req,res) => {
+    res.json(todos);
+});
+
+app.get('/todos/:id', (req,res) => {
+    var todoid = parseInt(req.params.id);
+    var matchedTodo;
+    todos.forEach(element => {
+        if(todoid ===  element.id) {
+            matchedTodo = element;
+        }
+    });
+    if(matchedTodo) {
+        res.json(matchedTodo);
+    }
+    else {
+        res.status(404).send();
+    }
+})
 
 app.listen(PORT, () => {
     console.log('App started on ' + PORT);
