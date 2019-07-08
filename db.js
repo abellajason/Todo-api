e70b0cf7a8abe1708162e60d049ce80cec0b5cac
jason <jason@snapzio.com>
2019-07-08 08:24:17 +0800

    changes

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ var env = process.env.NODE_ENV || 'development';
 var sequelize;
 
 if(env === 'production') {
-    sequelize = new Sequelize("process.env.DATABASE_URL", {
+    sequelize = new Sequelize("postgres://postgres:postgres@localhost/urxfqucqjebtsz", {
         'dialect' : 'postgres'
     })
 } else {
